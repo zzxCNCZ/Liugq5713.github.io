ecfdc7560fbed7b13b400c4e3b87b63116cd9931
liugq5713 <liugq5713@gmail.com>
2019-08-08 16:17:18 +0800

    fix

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,14 +1,27 @@
 module.exports = {
-  title: '重学前端',
-  base: '/fe/',
+  title: "重学前端",
+  base: "/fe/",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/favicon.ico"
+      }
+    ]
+  ],
   themeConfig: {
+    logo: "/hero.png",
     displayAllHeaders: true,
     sidebarDepth: 2,
-    lastUpdated: 'Last Updated',
-    nav: [{ text: 'JAVASCRIPT', link: '/javascript/' }, { text: 'WEBAPI', link: '/guide/' }],
+    lastUpdated: "Last Updated",
+    nav: [
+      { text: "JAVASCRIPT", link: "/javascript/" },
+      { text: "WEBAPI", link: "/guide/" }
+    ],
     sidebar: {
-      '/javascript/': ['', 'variable', 'type-common'],
-      '/': ['']
+      "/javascript/": ["", "variable", "type-common"],
+      "/": [""]
     }
   }
-}
+};
