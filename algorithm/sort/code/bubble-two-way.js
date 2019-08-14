9c4bee4f25930439da0cba3e960c02d23465b151
liugq <liugq5713@gmail.com>
2019-08-14 11:41:19 +0800

    create page

--- algorithm/sort/code/bubble-two-way.js
+++ algorithm/sort/code/bubble-two-way.js
@@ -1,1 +1,14 @@
-export const bubbleWithTwoWay = () => {};
+export const bubbleWithTwoWay = arr => {
+  let start = 0;
+  let end = arr.length - 1;
+  while(start < end){
+    for(let i =start ;i<end;i++){
+
+    }
+    start =1
+    for(let j=end ){
+
+    }
+    end =1
+  }
+};
