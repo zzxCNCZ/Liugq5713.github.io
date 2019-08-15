947e711f0a751fb572eed26ba5fb090a5f113689
liugq <liugq5713@gmail.com>
2019-08-15 10:55:52 +0800

    create page

--- algorithm/sort/code/bubble-base.js
+++ algorithm/sort/code/bubble-base.js
@@ -1,8 +1,8 @@
 export const bubble = arr => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
   }
