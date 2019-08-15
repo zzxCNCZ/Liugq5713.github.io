c31e0def1c4fc58f48eea433aec05e0a45302cb8
liugq <liugq5713@gmail.com>
2019-08-15 11:04:18 +0800

    create page

--- algorithm/sort/code/bubble-sort.js
+++ algorithm/sort/code/bubble-sort.js
@@ -1,1 +1,10 @@
-const bubbleSort = () => {};
+export const bubble = (arr, compareFunc) => {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (compareFunc(arr[j], arr[j + 1])) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
