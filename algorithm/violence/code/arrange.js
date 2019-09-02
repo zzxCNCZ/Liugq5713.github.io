624831bda9b5ec47eadada5d91d5722f31730c0f
liugq <liugq5713@gmail.com>
2019-09-02 18:30:53 +0800

    create page

--- algorithm/violence/code/arrange.js
+++ algorithm/violence/code/arrange.js
@@ -1,22 +1,43 @@
-function permutate (str) {
-  // 保存每一轮递归的排列结果
-  var result = []
-  // 初始条件：长度为1
-  if (str.length == 1) {
-    return [str]
-  } else {
-    // 求剩余子串的全排列,对每个排列进行遍历
-    var preResult = permutate(str.slice(1))
-    console.log('preResult', preResult)
-    for (var j = 0; j < preResult.length; j++) {
-      for (var k = 0; k < preResult[j].length + 1; k++) {
-        // 将首字母插入k位置
-        var temp = preResult[j].slice(0, k) + str[0] + preResult[j].slice(k)
-        result.push(temp)
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  let res = []
+  if (nums.length === 1) {
+    return [nums]
+  }
+  let preNums = permute(nums.slice(1))
+  for (let i = 0; i < preNums.length; i++) {
+    for (let j = 0; j <= preNums[i].length; j++) {
+      let p = preNums[i].slice(0)
+      p.splice(j, 0, nums[0])
+      res.push(p)
+    }
+  }
+  return res
+};
+console.log(permutate([1, 2, 3]))
+
+
+var permute = function (nums) {
+  let results = [];
+
+  let permutations = (current, remaining) => {
+    if (remaining.length <= 0) results.push(current.slice());
+    else {
+      // Loop through remaining elements
+      for (let i = 0; i < remaining.length; i++) {
+        // Insert the iTH element onto the end of current
+        current.push(remaining[i]);
+        // Recurse with inserted element removed
+        permutations(current.slice(), remaining.slice(0, i).concat(remaining.slice(i + 1)));
+        // Remove last inserted element for next iteration
+        current.pop();
       }
     }
-    return result
-  }
-}
+  };
 
-console.log(permutate('123'))
+  permutations([], nums);
+  return results;
+};
