b96f531611c6b5d2a8a46d51fc5ab3ac7f0b2670
liugq <liugq5713@gmail.com>
2019-10-30 11:11:51 +0800

    i am lazy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -132,7 +132,7 @@ module.exports = {
         {
           title: '题目',
           collapsable: false,
-          children: ['', 'search', 'dp', '13RomanToInteger', '17LetterCombinationsOfAPhoneNumber', 'link']
+          children: ['', 'search', 'dp', '13RomanToInteger', '17LetterCombinationsOfAPhoneNumber', 'link', 'tree']
         }
       ],
       '/': ['']
